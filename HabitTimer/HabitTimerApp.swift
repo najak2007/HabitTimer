31210324d5d3e878a03eddfceba057ce51c06955
najak2007 <najak2007@gmail.com>
2025-11-04 20:50:02 +0900

    First Project

---
+++ HabitTimer/HabitTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTimerApp.swift
+//  HabitTimer
+//
+//  Created by 오션블루 on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
