ae65e5f608422e6a48790fcac8bc6fa63216317e
najak2007 <najak2007@gmail.com>
2025-11-05 21:19:16 +0900

    타이머 Circle 로 해서 채우는 UI/UX 개발

--- HabitTimer/ContentView.swift
+++ HabitTimer/ContentView.swift
@@ -6,19 +6,45 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    private let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    private let startTime: Date = Date()
+    private let endTime: Double = 10
+    
+    @State private var elapsedTime: Double = 0.0
+    
+    // UI Config
+    private let themeColor: Color = Color(red: 252/255, green: 95/255, blue: 163/255)
+    private let circlePadding: CGFloat = 30
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Najak!")
-        }
-        .padding()
-    }
-}
+            Circle()
+                .strokeBorder(lineWidth: 24)
+                .overlay {
+                    Circle()
+                        .trim(from: 0.0, to: elapsedTime/endTime)
+                        .stroke(themeColor, style: StrokeStyle(lineWidth: 24.0, lineCap: .round, lineJoin: .round))
+                        .rotationEffect(Angle(degrees: 270))
+                        .animation(.easeInOut(duration: 1.0), value: elapsedTime)
+                        .padding(12)
+                }
+                .padding(circlePadding)
 
-#Preview {
-    ContentView()
+            
+        }
+        .onReceive(timer) { _ in
+            let elapsedTime = Date().timeIntervalSinceReferenceDate - startTime.timeIntervalSinceReferenceDate
+            if elapsedTime < endTime {
+                self.elapsedTime = elapsedTime
+            } else  {
+                self.elapsedTime = endTime
+            }
+        }
+        .onDisappear {
+            timer.upstream.connect().cancel()
+        }
+    }
 }
