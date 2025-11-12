343871d04c9b72b2f89bda99b25fc68002b9da18
najak2007 <najak2007@gmail.com>
2025-11-12 20:55:02 +0900

    편집과 작성을 Navi에 추가

--- HabitTimer/MainContentView.swift
+++ HabitTimer/MainContentView.swift
@@ -15,6 +15,9 @@ struct PostitListView: View {
  
     @StateObject private var toDoListViewModel = ToDoListViewModel()
     @State private var isEditMode: Bool = false
+    @State private var messageText: String = ""
+    @State private var isFocused: Bool = false
+    @State private var inputHeight: CGFloat = 42
     
     let coloredNavAppearance = UINavigationBarAppearance()
     
@@ -39,26 +42,78 @@ struct PostitListView: View {
                             Text(toDoListViewModel.toDoList[index].messageText)
                                 .font(.custom("GmarketSansTTFMedium", size: 24))
                                 .frame(width: 300, height: 300)
-                                .id(toDoListViewModel.toDoList[index].id)
+                                
                         }
                         .listRowSeparator(.hidden)
                         .listRowBackground(Color.white)
                         .listRowInsets(EdgeInsets())
+                        .id(toDoListViewModel.toDoList[index].id)
                     }
                 }
+                .onChange(of: toDoListViewModel.toDoList.count) { oldValue, newValue in
+                    if let lastToDoItem = toDoListViewModel.toDoList.last {
+                        withAnimation {
+                            proxy.scrollTo(lastToDoItem.id, anchor: .bottom)
+                        }
+                    }
+                }
+                .onAppear {
+                    if let lastToDoItem = toDoListViewModel.toDoList.last {
+                        withAnimation {
+                            proxy.scrollTo(lastToDoItem.id, anchor: .bottom)
+                        }
+                    }
+                }
+                
                 .scrollContentBackground(.hidden)
                 .background(.white)
                 .contentMargins(.horizontal, 0)
-                .padding(.top, 0)
+                .padding(.top, -32)
+                
+//                HStack(spacing: 10) {
+//                    VStack(alignment: .leading, spacing: 0) {
+//                        UITextViewRepresentable(text: $messageText, isFocused: $isFocused, inputHeight: $inputHeight)
+//                            .frame(height: inputHeight)
+//                    }
+//                    
+//                    Button(action: {
+//                        guard messageText.isEmpty == false else { return }
+//                        let trimString = messageText.trimmingCharacters(in: .whitespacesAndNewlines)
+//                        guard trimString.isEmpty == false else { return }
+//                        
+//                        let newToDoData = ToDoListData()
+//                        newToDoData.setMessageText(messageText: trimString)
+//                        toDoListViewModel.addToDoList(newToDoData)
+//                        messageText = ""
+//                    }, label: {
+//                        Image(systemName: "arrowshape.up.circle.fill")
+//                            .resizable()
+//                            .frame(width: 40, height: 40)
+//                            .foregroundColor(Color("1F2020"))
+//                    })
+//                }
+//                .padding(.horizontal, 10)
+//                .padding(.bottom, 10)
+//                .background(.clear)
             }
             .navigationBarTitleDisplayMode(.inline)
             .navigationBarItems(
-                trailing: Button(action: {
+                leading: Button(action: {
                     
                 }, label: {
                     Text("편집")
-                        .font(.custom("GmarketSansTTFMedium", size: 18))
+                        .font(.custom("GmarketSansTTFMedium", size: 16))
                         .foregroundColor(.black)
+                })
+                .buttonStyle(PlainButtonStyle()),
+                
+                trailing: Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "square.and.pencil")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(Color("1F2020"))
                 })
                 .buttonStyle(PlainButtonStyle())
             )
